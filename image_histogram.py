23e007aa9ee2f8d14e659ac24093443764742c26
Paul Aljabar <m.mouse@disney.com>
2016-01-13 18:37:55 +0000

    Option to set right hand range of histogram.

--- image_histogram.py
+++ image_histogram.py
@@ -20,6 +20,9 @@ def main(*args):
 
   helpText = 'Number of bins: default = 30'
   parser.add_argument("-bins", type=int, help=helpText, default=30)
+
+  helpText = 'Maximum on x-axis'
+  parser.add_argument("-xMax", type=int, help=helpText, default=30)
   
   helpText = 'Output: Don\'t show plot, save it to a file with given name.'
   parser.add_argument('-output', type=str, help=helpText, default=None)
@@ -32,14 +35,21 @@ def main(*args):
   threshold = args.threshold
   nBins = args.bins
   outputName = args.output
-  
+  xMax = args.xMax
+
+
   img = nib.load(inputFilename)
   imgDType = img.get_data_dtype()
 
   imgData = img.get_data().astype(imgDType)
   
   pyplot.hist(imgData[imgData > threshold], bins=nBins)
-  
+  lims = pyplot.axis()
+
+  if not (xMax == None):
+    xmin, oldXMax = pyplot.xlim()
+    pyplot.xlim( (xmin, xMax) )
+
   if outputName == None:
     pyplot.show()
   else:
